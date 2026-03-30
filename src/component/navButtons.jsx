16d7313b6c4e7b3f1e1e71c6217cbd9bab2983ed
Bryan Mejia <mejiab2@gmail.com>
2026-03-29 21:07:00 -0400

    added three new side navigation button first top button is fully working with img and video

--- src/component/navButtons.jsx
+++ src/component/navButtons.jsx
@@ -1,11 +1,19 @@
 import { useState, useMemo } from "react";
+import RefrigerationCycle from "./reviewPages/refrigerationCycle"
 import "./navButtons.css"
 
 
 export default function NavButtons( {page, setPage}) {
+
+    if (page === "RefrigerationCycle") {
+    return <RefrigerationCycle setPage={setPage} />;
+  }
+   
     return (
         <div className="navBtn">
-            <button onClick={()=> {}}> Refrigeration Cycle</button>
+            <button  onClick={()=> {setPage("RefrigerationCycle")}}> Refrigeration Cycle</button>
+            <button  onClick={()=> {setPage("fireSafety")}}> Fire Life Safety</button>
+            <button  onClick={()=> {setPage("generator")}}> Generator</button>
         </div>
 
     )
